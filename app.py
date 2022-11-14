73c0a49d03ebd150ffb261712512f8796698802d
Manuel Šarić <manujelko@gmail.com>
2022-11-14 19:08:53 +0100

    Get a specific store and its items

--- app.py
+++ app.py
@@ -37,3 +37,19 @@ def create_item(name):
             store["items"].append(new_item)
             return new_item
     return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
