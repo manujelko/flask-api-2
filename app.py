a35a2a429f2e066148bcefaecc4a114e5f802b08
Manuel Šarić <manujelko@gmail.com>
2022-11-16 19:22:30 +0100

    One to many relationship between stores and tags

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask
 from flask_smorest import Api
 
 from db import db
-import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 
 def create_app(db_url=None):
@@ -32,5 +32,6 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 from .item import ItemModel
 from .store import StoreModel
+from .tag import TagModel
 
 __all__ = [
     "ItemModel",
     "StoreModel",
+    "TagModel",
 ]
