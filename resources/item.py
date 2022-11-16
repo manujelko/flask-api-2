c9db0a9fc0ac4dea202e10426fdce9c7a31c4616
Manuel Šarić <manujelko@gmail.com>
2022-11-16 16:18:13 +0100

    Update models

--- resources/item.py
+++ resources/item.py
@@ -26,8 +26,17 @@ class Item(MethodView):
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Updating an item is not implemented.")
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(**item_data, id=item_id)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
 
 
 @blp.route("/item")
