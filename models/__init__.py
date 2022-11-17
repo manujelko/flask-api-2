fe31ac564af059542ec7d12dd8e159674428b589
Manuel Šarić <manujelko@gmail.com>
2022-11-17 17:34:23 +0100

    Add user model and schema.

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,12 @@ from .item import ItemModel
 from .items_tags import ItemsTags
 from .store import StoreModel
 from .tag import TagModel
+from .user import UserModel
 
 __all__ = [
     "ItemModel",
     "ItemsTags",
     "StoreModel",
     "TagModel",
+    "UserModel",
 ]
